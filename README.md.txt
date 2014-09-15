b707012178a0765cb6500b419a8ddfe5c1a1a416
Embedit Electronics <embedit@embeditelectronics.com>
2014-09-15 18:30:41 -0400

    first commit

---
+++ README.md.txt
@@ -0,0 +1,14 @@
+The Scratch visual programming language brought to the Raspberry Pi and RPiSoC!
+This is an extension of Scratch GPIO 5: http://cymplecy.wordpress.com/scratchgpio/scratch-raspberrypi-gpio/
+
+To use this follow these installation steps:
+
+1. First you need our psoc_2_pi Python API set up. SPI support only for now Instructions here: http://embeditelectronics.github.io/psoc_2_pi/getting_started.html#configure-spi-and-download-spidev
+
+2. Next you need to install Scratch GPIO: http://cymplecy.wordpress.com/scratchgpio/scratch-raspberrypi-gpio/
+
+3. Finally download this repository and put the files into the scratchgpio5 folder, overwriting any existing files. The path will should be /home/pi/scratchgpio5
+
+4. Open up the ScratchGPIO 5 icon from the desktop and enjoy. A short tutorial for using it can be found here: http://www.embeditelectronics.com/blog/rpisoc/creating-your-first-scratch-program/
+
+I will put some example projects in this repo as well
---
+++ sgh_RasPiCamera.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# sgh_RasPiCamera - control the raspberry pi camera via scratch
+#Copyright (C) 2014 by Matt Venn
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Version =  '0.0.1sw5' # 22May14 Add Try except arroudn take pic code
+
+import os,glob,pwd,grp
+
+class RasPiCamera:
+
+    def __init__(self):
+        print "pi camera init"
+        self.num = 0
+
+        self.user = os.getenv("SUDO_USER")
+        self.dir = ("/home/"+ self.user + "/photos/")
+
+        try:
+            os.mkdir(self.dir)
+        except OSError:
+            #already exists
+            pass
+
+        photos = glob.glob(self.dir + '*.jpg')
+        if len(photos):
+            latest_photo = sorted(photos)[-1]
+            latest_photo = latest_photo.replace(self.dir,'')
+            latest_photo = latest_photo.replace('.jpg','')
+            latest_photo_num = int(latest_photo)
+            self.num = latest_photo_num + 1
+
+        print("starting at %d" % self.num)
+
+
+    def take_photo(self):
+        try:
+            photo_file = self.dir + str(self.num) + '.jpg'
+            os.system("raspistill -n -t 1 -o " + photo_file)
+            #chown the photo so it can be modified by the user
+            uid = pwd.getpwnam(self.user).pw_uid
+            #assume group is same name as user
+            gid = grp.getgrnam(self.user).gr_gid
+            os.chown(photo_file, uid, gid)
+            print "photo taken: " + photo_file
+            self.num += 1
+        except:
+            print "Error taking photo - camera probably not correctly fitted"
+            pass
+
+
+
+
+#### end RasPiCamera ###############################################################
+
+
+
+
